740b0a6d1e8b7dc15c7028cc20857fadecdfa0d4
pablormacia <pablo-macia@live.com.ar>
2022-06-15 15:59:18 -0300

    Agregado Nav y Logo

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logo.png"
+
+const NavBar = ()=>{
+    return (
+        <>
+            <nav><img src={logo}></img>Soy el navbar</nav>
+        </>
+    )
+}
+
+
+export default NavBar
