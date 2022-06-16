485646cd84b9382837a6ad5f2c08f18ccf400bf1
pablormacia <pablo-macia@live.com.ar>
2022-06-16 16:19:59 -0300

    Working in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,34 @@
 import React from "react";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
+import "./navBar.module.scss";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
 
 const NavBar = ()=>{
     return (
         <>
-            <nav><img src={logo}></img>Soy el navbar</nav>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                    <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                    </IconButton>
+                    <Avatar alt="Remy Sharp" src={logo} />
+                    <Button href="#" variant="h6" underline="none" color="#FFF">Home</Button>
+                    <Button href="#" variant="h6" underline="none" color="#FFF">Contacto</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </>
     )
 }
+
 
 
 export default NavBar
